b74630c8fcc0c0a48810424030e4322fe60cdb05
Joonas Harjukallio <harjukallio@gmail.com>
2019-03-26 11:17:16 +0200

    Add Post entity and PostRepository

--- src/main/java/com/example/springbootblog/entities/Post.java
+++ src/main/java/com/example/springbootblog/entities/Post.java
@@ -3,8 +3,18 @@ package com.example.springbootblog.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Post {
+    private @Id @GeneratedValue Long id;
+    private String title;
+    private String body;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
 }

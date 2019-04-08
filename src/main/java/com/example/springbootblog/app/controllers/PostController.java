44c01639036b6316af0fd6eb62586ea361ca5e53
Joonas Harjukallio <harjukallio@gmail.com>
2019-04-08 15:02:47 +0300

    Add endpoint to add comments for a post

--- src/main/java/com/example/springbootblog/app/controllers/PostController.java
+++ src/main/java/com/example/springbootblog/app/controllers/PostController.java
@@ -1,14 +1,12 @@
 package com.example.springbootblog.app.controllers;
 
+import com.example.springbootblog.app.entities.Comment;
 import com.example.springbootblog.app.exceptions.EntityNotFound;
 import com.example.springbootblog.app.entities.Post;
 import com.example.springbootblog.app.repositories.PostRepository;
 import com.example.springbootblog.app.views.posts.PostIndexView;
 import com.example.springbootblog.app.views.posts.PostShowView;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -31,8 +29,18 @@ public class PostController {
         return new PostShowView(postRepository.findById(id).orElseThrow(() -> new EntityNotFound(id)));
     }
 
-    @GetMapping("/without_comments/{id}")
-    Post showWithoutComments(@PathVariable Long id) {
+    @GetMapping("/without_relationships/{id}")
+    Post showWithoutRelationShips(@PathVariable Long id) {
         return postRepository.findById(id).orElseThrow(() -> new EntityNotFound(id));
+    }
+
+    @PostMapping("/add_comment/{id}")
+    PostShowView addComment(@PathVariable Long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new EntityNotFound(id));
+        int numberOfComments = post.getComments().size() + 1;
+        Comment comment = new Comment("Comment " + numberOfComments);
+        post.addComment(comment);
+
+        return new PostShowView(postRepository.save(post));
     }
 }
